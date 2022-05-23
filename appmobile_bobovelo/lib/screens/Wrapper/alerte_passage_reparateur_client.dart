eb8f271ee0645f73a3ef5751f87e7d844d63dc47
Madi niakate <madiniakate6991@gmail.com>
2022-05-23 22:26:26 +0200

    new change 2

--- appmobile_bobovelo/lib/screens/Wrapper/alerte_passage_reparateur_client.dart
+++ appmobile_bobovelo/lib/screens/Wrapper/alerte_passage_reparateur_client.dart
@@ -3,60 +3,68 @@ import 'package:appmobile_bobovelo/screens/Wrapper/home/reparateur/addServicePag
 import 'package:flutter/material.dart';
 
 class PassageClientReparateur extends StatefulWidget {
-   final repairorclient;
-   final client;
-  const PassageClientReparateur({Key? key, required this.repairorclient, required this.client }) : super(key: key);
+  final repairorclient;
+  final client;
+  const PassageClientReparateur(
+      {Key? key, required this.repairorclient, required this.client})
+      : super(key: key);
 
   @override
-  State<PassageClientReparateur> createState() => _PassageClientReparateurState();
+  State<PassageClientReparateur> createState() =>
+      _PassageClientReparateurState();
 }
 
 class _PassageClientReparateurState extends State<PassageClientReparateur> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Center(
-          child: Container(
-            color: Colors.black,
-            height: 100,
-            width: 400,
-            padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-            child: Column(
-              children: [
-                Align( alignment: Alignment.center,child: Text(widget.repairorclient, style: TextStyle(color: Colors.white),)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+        body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: Center(
+        child: Container(
+          color: Colors.black,
+          height: 100,
+          width: 400,
+          padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    widget.repairorclient,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 13),
+                  )),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
                   ElevatedButton(
-                    onPressed: (){
-                       Navigator.push(context,
-                            MaterialPageRoute(
-                              builder: (context) => const AddServicePage()
-                            ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddServicePage()),
                         );
-                    },
-                    child: widget.client? Text('Oui') : Text('Non')
-                  ),
+                      },
+                      child: widget.client ? Text('Oui') : Text('Non')),
                   ElevatedButton(
-                    onPressed: (){
-                       Navigator.push(context,
-                            MaterialPageRoute(
-                              builder: (context) => HomePage()
-                            ),
-                );
-                    },
-                    child: widget.client? Text('Non') : Text('Oui')
-                  )
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: widget.client ? Text('Non') : Text('Oui'))
                 ],
-                )
-              ],
-            ),
+              )
+            ],
+          ),
         ),
-    ),
-      ));
+      ),
+    ));
   }
 }
